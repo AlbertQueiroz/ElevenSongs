b0a8b6ab67cb9424a0e02a9df9926d739f4dc45a
Albert Queiroz <albert@mobills.com.br>
2024-09-03 17:16:58 -0300

    feature (Library): create library and favorites tab sections

---
+++ ElevenSongs/ElevenSongsApp.swift
@@ -0,0 +1,32 @@
+//
+//  ElevenSongsApp.swift
+//  ElevenSongs
+//
+//  Created by Albert on 03/09/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ElevenSongsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
